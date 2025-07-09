c11c6d46d9e0697d085637ac1741aa3d2c086fa6
Wasel Kirmany <waselkirmany25@gmail.com>
2025-07-09 16:02:57 +0530

    testfailed

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ import "./style.scss";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
-  // const { currentUser } = useContext(AuthContext);
-  const { currentUser } = true; // Simulating no user for testing purposes
+  const { currentUser } = useContext(AuthContext);
+  // const { currentUser } = true; // Simulating no user for testing purposes
   useEffect(() => {
     document.body.className = darkMode ? "theme-dark" : "theme-light";
   }, [darkMode]);
